c2ee2857d264c3b41db4c858797c1424007ee95b
greenwood99155 <greenwood99155@gmail.com>
2020-08-12 12:55:09 +0200

    blogs friendly_id slugs, enum for stage published/draft

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,6 +3,10 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
   def new
     @portfolio_item = Portfolio.new
   end
@@ -32,6 +36,15 @@ class PortfoliosController < ApplicationController
       else
         format.html { render :edit }
       end
+    end
+  end
+
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item.destroy
+
+    respond_to do |format|
+      format.html { redirect_to portfolios_path, notice: "Portfolio item was successfully destroyed." }
     end
   end
 
